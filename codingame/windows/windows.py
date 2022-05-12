43c9ed93aeee0b05ee053278f5240c16386356f6
Montassar Ben Dhifallah <montassar.bendhifallah.dg@gmail.com>
2022-05-12 08:38:07 +0100

    Update windows.py

--- codingame/windows/windows.py
+++ codingame/windows/windows.py
@@ -32,21 +32,22 @@ def window():
     # pane length
     n = int(input())
 
-    window = ""
     # window head and footer
     top = "-" * (n*2+3)
     
-    lines = ""
-    
+    # construct window pane lines (window upper half)
     for i in range(n):
         dots=""
         for j in range(n):
             dots+="."
         lines += "|" + dots +"|" + dots + "|\n"
-        
+    
+    # add middle separator
     middle = "|" + "-" * n + "+" + "-" * n + "|\n"
-
-    w += top+ "\n" + lines + middle + lines + top
+    
+    # combine components
+    window += top+ "\n" + lines + middle + lines + top
+    
     print(window)
 
 if __name__ == '__main__':
